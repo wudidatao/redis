dea2f125ebc16e3b754c0c332c051f69a3bba24a
wudidatao <82302925@qq.com>
2018-01-18 23:19:39 +0800

    20180118

--- redis_install.sh
+++ redis_install.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# redis°²×°
+. ./base.sh
 
+if [ ! -e $path/redis-$version.tar.gz ];then
+    cd $path
+    wget http://download.redis.io/releases/redis-$version.tar.gz
+    tar -zxvf redis-$version.tar.gz
+fi
+
+cd $path/redis-$version
+make && make install
