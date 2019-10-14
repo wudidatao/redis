1e50ff9a5543d12f92511c281d62505af4d1cb47
wudidatao <82302925@qq.com>
2019-10-14 16:10:44 +0800

    Update redis_zabbix_discovery.sh

--- redis_zabbix_discovery.sh
+++ redis_zabbix_discovery.sh
@@ -35,11 +35,11 @@ echo "UserParameter=redis_stats[*],$redis_cli_path -h $localhost -a $redis_passw
 UserParameter=redis.discovery,python $zabbix_conf_path/redis_port.py
 
 #自动发现慢查询日志
-UserParameter=redis_slowlog_len_max[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 CONFIG GET slowlog-max-len |sed -n 2p
-UserParameter=redis_slowlog_len[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 SLOWLOG LEN |cut -d : -f2
-UserParameter=redis_slowlog_last[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 SLOWLOG GET 1 |cut -d : -f2|xargs -l7
-UserParameter=redis_slowlog_slower_than[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 CONFIG GET slowlog-log-slower-than |sed -n 2p
+UserParameter=redis_slowlog_len_max[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 CONFIG GET slowlog-max-len 2>/dev/null|sed -n 2p
+UserParameter=redis_slowlog_len[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 SLOWLOG LEN 2>/dev/null|cut -d : -f2
+UserParameter=redis_slowlog_last[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 SLOWLOG GET 1 2>/dev/null|cut -d : -f2|xargs -l7
+UserParameter=redis_slowlog_slower_than[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 CONFIG GET slowlog-log-slower-than 2>/dev/null |sed -n 2p
 
 #当前keys数
-UserParameter=redis_db0_keys[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 info | grep db0 |cut -d '=' -f2 |cut -d ',' -f1
+UserParameter=redis_db0_keys[*],$redis_cli_path -h $localhost -a $redis_password -p \$1 info 2>/dev/null| grep db0 |cut -d '=' -f2 |cut -d ',' -f1
 " > userparameter_redis.conf
